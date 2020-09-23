f4eecbbf1f1b2d8152be1d8a57389c2ecfb3d285
Aditi Kumari <aditik@juniper.net>
2020-09-23 10:26:28 -0700

    uwsgi api server entrypoint
    Closes-Jira-Bug: CEM-19336
    
    Root Cause: No entrypoint to start multiple workers of api-server with different config files
    while deploying api server with uwsgi
    
    Solution: This change introduces the uwsgi_api_server.py to work with uwsgi deployment.
    
    Signed-off-by: Aditi Kumari <aditik@juniper.net>
    Change-Id: I245d7cced2eb6b54b47e10cda5891a29ba6f5a30

---
+++ src/config/api-server/vnc_cfg_api_server/uwsgi_api_server.py
@@ -0,0 +1,17 @@
+import uwsgi
+import api_server
+from gevent import monkey
+monkey.patch_all()
+
+
+def get_apiserver():
+    worker_id = int(uwsgi.worker_id())
+    if worker_id > 0:
+        vnc_api_server = api_server.VncApiServer(
+            "-c /etc/contrail/contrail-api-%s.conf"
+            "-c /etc/contrail/contrail-keystone-auth.conf" %
+            (worker_id - 1))
+    pipe_start_app = vnc_api_server.get_pipe_start_app()
+    if pipe_start_app is None:
+        pipe_start_app = vnc_api_server.api_bottle
+    return pipe_start_app
