14d1f6fa3a7eaa6a23c2c52cc8de7866fac3dd28
Andrey Pavlov <andrey-mp@yandex.ru>
2020-09-26 10:40:02 +0300

    fix incorrect method call in delete IP operation
    closes-jira-bug: CEM-19425
    Change-Id: Ie9ce7fa22079a0bb4a24d395359818a168aa31dc

--- src/container/kube-manager/kube_manager/vnc/vnc_pod.py
+++ src/container/kube-manager/kube_manager/vnc/vnc_pod.py
@@ -583,7 +583,9 @@ class VncPod(VncCommon):
                 self._vnc_lib.ref_update('floating-ip', fip_id,
                                          'virtual-machine-interface', vmi_id, None,
                                          'DELETE')
-                FloatingIpKM.update(fip_id)
+                fip_obj = FloatingIpKM.get(fip_id)
+                if fip_obj:
+                    fip_obj.update()
             except NoIdError:
                 pass
 
