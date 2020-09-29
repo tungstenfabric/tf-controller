d83b0c5953cd88a3b6b8bc0d0ac27a38b4954079
Nishant Sankhala <snishant@juniper.net>
2020-09-29 17:30:27 +0530

    [Fabric] - Bug fixes for ZTP failures due to fpc errors
    CEM-17894: Doesn't fail the ZTP if fpc/pic is empty
    CEM-16553: Doesn't fail the ZTP if fpc HW error
    
    updates job log with failed fpc/pic status in above cases and continues.
    
    Closes-jira-bug: CEM-17894
    Closes-jira-bug: CEM-16553
    Change-Id: Icde615107c6ef41ff4bbff7d232c832add74fa81
    (cherry picked from commit e77140d45f0a534557457837a1497ef8f2596a53)

--- src/config/fabric-ansible/ansible-playbooks/roles/wait_for_fpc_online/filter_plugins/fpc_filters.py
+++ src/config/fabric-ansible/ansible-playbooks/roles/wait_for_fpc_online/filter_plugins/fpc_filters.py
@@ -16,25 +16,38 @@ class FilterModule(object):
 
     @classmethod
     def is_juniper_device_online(cls, fpc_pic_status_info):
+        msg = list()
+        out = dict()
         if not fpc_pic_status_info or \
                 not fpc_pic_status_info.get('fpc-information') or \
                 not fpc_pic_status_info.get('fpc-information').get('fpc'):
-            return False
+            out['status'] = False
+            out['msg'] = 'failed to get fpc-information'
+            return out
         fpc_slots = fpc_pic_status_info.get('fpc-information').get('fpc')
         if isinstance(fpc_slots, dict):
             fpc_slots = [fpc_slots]
         for fpc in fpc_slots:
             if not fpc.get('state') or fpc.get('state').lower() != 'online':
-                return False
+                msg.append("fpc at slot {0} is {1}".format(fpc.get('slot', ''),
+                fpc.get('state', 'Offline')))
+                continue
             pic_slots = fpc.get('pic')
             if pic_slots is None:
-                return False
+                msg.append("fpc at slot {0} has no pics".format(
+                    fpc.get('slot', '')))
+                continue
             if isinstance(pic_slots, dict):
                 pic_slots = [pic_slots]
             for pic in pic_slots:
                 if not pic.get('pic-state') or \
                         pic.get('pic-state').lower() != 'online':
-                    return False
-        return True
+                    msg.append("pic at pic-slot {0} of fpc slot {1} is {2}".format(
+                        pic.get('pic-slot', ''), fpc.get('slot', ''),
+                        pic.get('state', 'Offline')))
+                    continue
+        out['status'] = True
+        out['msg'] = ('; '.join(msg))
+        return out
     # end is_juniper_device_online
 # end FilterModule
