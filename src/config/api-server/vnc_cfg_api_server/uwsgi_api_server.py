d34c5f784ee01ce1b4c61f6706cbb2bc18012f75
alexey-mr <alexey.morlang@gmail.com>
2020-10-05 19:50:36 +0300

    Fixed config passig to api server wor uwsgi case
    partial-jira-bug: CEM-19232
    Change-Id: I6db2330d09707122a5d4059a677100dca1fc1af2

--- src/config/api-server/vnc_cfg_api_server/uwsgi_api_server.py
+++ src/config/api-server/vnc_cfg_api_server/uwsgi_api_server.py
@@ -8,8 +8,8 @@ def get_apiserver():
     worker_id = int(uwsgi.worker_id())
     if worker_id > 0:
         vnc_api_server = api_server.VncApiServer(
-            "-c /etc/contrail/contrail-api-%s.conf"
-            "-c /etc/contrail/contrail-keystone-auth.conf" %
+            " -c /etc/contrail/contrail-api-%s.conf"
+            " -c /etc/contrail/contrail-keystone-auth.conf" %
             (worker_id - 1))
     pipe_start_app = vnc_api_server.get_pipe_start_app()
     if pipe_start_app is None:
