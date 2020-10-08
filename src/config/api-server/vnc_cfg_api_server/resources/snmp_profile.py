b0421b70dffa0dacd39704bda9ae88faf0840c21
akshayam <akshayam@juniper.net>
2020-10-08 19:36:11 +0000

    [API] API config validations for TCM
    Telemetry Configuration Management involves creating 3 sub profiles and
    updates to existing Telemetry Profile. Including api server checks for the
    below objects:
    
    1. Telemetry Profile
    2. Grpc Profile
    3. Netconf Profile
    4. Snmp Profile
    
    Change-Id: I930eb3a6ae0b78bae4ece2a0adcb20b6e169700f
    closes-jira-bug: CEM-19046
    target-release: R2011

---
+++ src/config/api-server/vnc_cfg_api_server/resources/snmp_profile.py
@@ -0,0 +1,79 @@
+#
+# Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+#
+
+from vnc_api.gen.resource_common import SnmpProfile
+
+from vnc_cfg_api_server.resources._resource_base import ResourceMixin
+
+
+class SnmpProfileServer(ResourceMixin, SnmpProfile):
+    @staticmethod
+    def is_crud_allowed(is_default_profile, sp_fq_name):
+        if is_default_profile:
+            return (False, (400, "One of the following non-permitted "
+                                 "operations was attempted on "
+                                 "snmp profile %s: \n"
+                                 "1. Marking profile as default \n"
+                                 "2. Creating or Editing or Deleting a "
+                                 "default profile "
+                            % sp_fq_name))
+        return True, ''
+    # end is_crud_allowed
+
+    @staticmethod
+    def check_if_predefined(sp_fq_name):
+
+        predef_fq_names_list = [
+
+            ["default-domain", "default-project",
+             "snmp-default-profile"]
+        ]
+        return sp_fq_name in predef_fq_names_list
+    # end check_if_predefined
+
+    @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+
+        is_default_profile = obj_dict.get(
+            'snmp_profile_is_default')
+
+        sp_fq_name = obj_dict.get('fq_name')
+
+        # check if its a predefined default profile
+        # exempt predefined default profiles from crud check
+        is_predef_profile = cls.check_if_predefined(sp_fq_name)
+
+        if is_predef_profile:
+            return True, ''
+
+        return cls.is_crud_allowed(is_default_profile, sp_fq_name)
+    # end pre_dbe_create
+
+    @classmethod
+    def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
+
+        ok, db_obj_dict = db_conn.dbe_read(
+            obj_type='snmp_profile',
+            obj_id=obj_dict['uuid'],
+            obj_fields=['snmp_profile_is_default'])
+        if not ok:
+            return ok, (400, db_obj_dict)
+
+        is_default_profile = obj_dict.get(
+            'snmp_profile_is_default',
+            db_obj_dict.get('snmp_profile_is_default')
+        )
+        sp_fq_name = obj_dict.get('fq_name')
+        return cls.is_crud_allowed(is_default_profile, sp_fq_name)
+    # end pre_dbe_update
+
+    @classmethod
+    def pre_dbe_delete(cls, id, obj_dict, db_conn):
+
+        is_default_profile = obj_dict.get('snmp_profile_is_default')
+
+        sp_fq_name = obj_dict.get('fq_name')
+        ok, result = cls.is_crud_allowed(is_default_profile, sp_fq_name)
+        return ok, result, None
+    # end pre_dbe_delete
