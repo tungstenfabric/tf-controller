4481722ef5b024c499801b8dfd9bd733c16fd32f
Aditi Kumari <aditik@juniper.net>
2020-10-22 16:54:33 -0700

    Fix: Multiple api workers not working, uwsgi master is not serving requests
    Closes-Jira-Bug: CEM-19932
    
    Root Cause: Application loading in workers was blocked by DB resync operation and
    master was still accepting requests during this period.
    
    Solution: This change adds he check for not accepting requests while the application
    is getting loaded in the workers.
    
    Signed-off-by: Aditi Kumari <aditik@juniper.net>
    Change-Id: I491c4589b341131fecdd70ea3b342ea043946e26

--- src/config/api-server/vnc_cfg_api_server/uwsgi_api_server.py
+++ src/config/api-server/vnc_cfg_api_server/uwsgi_api_server.py
@@ -1,3 +1,4 @@
+import bottle
 import uwsgi
 import api_server
 from gevent import monkey
@@ -11,7 +12,19 @@ def get_apiserver():
             " -c /etc/contrail/contrail-api-%s.conf"
             " -c /etc/contrail/contrail-keystone-auth.conf" %
             (worker_id - 1))
+
     pipe_start_app = vnc_api_server.get_pipe_start_app()
     if pipe_start_app is None:
         pipe_start_app = vnc_api_server.api_bottle
+
+    @vnc_api_server.api_bottle.hook('before_request')
+    def check_db_resync_status():
+        try:
+            if not vnc_api_server._db_conn._db_resync_done.isSet():
+                err_msg = "Api server is Initializing:DB Resync in progress"
+                raise bottle.HTTPError(503, err_msg)
+        except AttributeError:
+            err_msg = "Api server is Initializing:DB Resync in progress"
+            raise bottle.HTTPError(503, err_msg)
+
     return pipe_start_app
