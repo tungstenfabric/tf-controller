00ce31c4a83de7fef3581e07471fb67e3aef3e1c
Manish Sahu <msahu@juniper.net>
2021-03-23 11:25:39 +0530

    Install inet route properly for evpn type2 route with proper stitched mac
    IP-MAC binding was not happening for routes with inet-evpn path,
    hence no stitched mac was present for the bms route in vrouter and
    thus ping was failing. Have added a check to return true in
    VrfKSyncObject::RouteNeedsMacBinding for route path with inet-evpn
    peer.
    
    closes-jira-bug: CEM-21083
    
    Change-Id: Ib0b9ebd1d0a3a640e25956594d83f98e17a8c4b8
    Signed-off-by: Manish Sahu <msahu@juniper.net>

--- src/vnsw/agent/vrouter/ksync/route_ksync.cc
+++ src/vnsw/agent/vrouter/ksync/route_ksync.cc
@@ -1160,6 +1160,11 @@ bool VrfKSyncObject::RouteNeedsMacBinding(const InetUnicastRouteEntry *rt) {
             return false;
     }
 
+    // Return true if route has inet evpn path
+    if (rt->FindInetEvpnPath()) {
+        return true;
+    }
+
     // If the route crosses a VN, we want packet to be routed. So, override
     const NextHop *nh = rt->GetActiveNextHop();
     if (nh == NULL)
