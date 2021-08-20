50a5010cb703478a3c3a7216ec7591c3977a6c89
Nagendra Maynattamai <npchandran@juniper.net>
2021-08-19 22:49:38 -0700

    Ignore unavailable imports in older releases closes-jira-bug: CEM-23502
    Change-Id: Ib425ecba8c2fff8f536230dc125c046755576ccb
    Signed-off-by: Nagendra Maynattamai <npchandran@juniper.net>

--- src/config/common/cfgm_common/db_json_exim.py
+++ src/config/common/cfgm_common/db_json_exim.py
@@ -15,9 +15,13 @@ from __future__ import unicode_literals
 # python db_json_exim.py --export-to <filename>
 
 
-from builtins import str
-from builtins import object
-from future.utils import native_str
+try:
+    from builtins import str
+    from builtins import object
+    from future.utils import native_str
+except ImportError as exc:
+    pass
+
 import sys
 if sys.version_info[0] < 3:
     reload(sys)
